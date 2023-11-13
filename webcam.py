27de9aaed4ea0971c979c5692a9d7a0dd22873bb
huyctdev <huyctdev@gmail.com>
2023-11-13 22:53:46 +0700

    update

--- webcam.py
+++ webcam.py
@@ -1,24 +1,32 @@
 # from PIL import Image
+import MySQLdb
 import cv2
 import torch
 import subprocess
 import imageio.v2 as imageio
 from moviepy.editor import VideoFileClip
+from requests_toolbelt.multipart.encoder import MultipartEncoder
 import base64
+from flask_sqlalchemy import SQLAlchemy
 # import math 
 import function.utils_rotate as utils_rotate
 # from IPython.display import display
 import os
-import datetime
+from datetime import datetime
 import numpy as np
 from flask import Flask, request, jsonify, make_response, send_file, Response
 from flask_cors import CORS, cross_origin
 import time
 # import argparse
 import function.helper as helper
+datetime.utcnow()
 
 app = Flask(__name__)
 CORS(app, origins=["https://datn-huy.vercel.app", "http://localhost:3000"])
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:Test@123456@localhost/graduation_db'
+# db = SQLAlchemy(app)
+db = MySQLdb.connect(host="localhost", user="root", passwd="Test@123456", db="graduation_db")
+cur = db.cursor()
 
 # load model
 yolo_LP_detect = torch.hub.load('yolov5', 'custom', path='model/LP_detector_nano_61.pt', force_reload=True, source='local')
@@ -27,6 +35,14 @@ yolo_license_plate.conf = 0.60
 
 prev_frame_time = 0
 new_frame_time = 0
+# class LicensePlate(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     queue_id = db.Column(db.Integer)
+#     lps = db.Column(db.String(255), unique=False, nullable=False)
+#     title = db.Column(db.String(255))
+#     created_at = db.Column(db.DateTime, default=datetime.utcnow)
+#     updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+
 
 @app.route("/", methods=["GET"])
 def getApp():
@@ -39,6 +55,10 @@ def detectLpVideo():
         return jsonify({"error": "No file part"}), 422
     
     video = request.files["video"]
+    queue_id = request.form.get('queue_id')
+    if not queue_id:
+        return jsonify({'message': 'Thiếu thông tin để thêm bản ghi mới'}), 400
+
 
     file_path = os.path.join(
         "temp",
@@ -56,11 +76,7 @@ def detectLpVideo():
     print('running...')
 
     vid = cv2.VideoCapture(file_path, cv2.CAP_FFMPEG)
-    # fourcc = cv2.VideoWriter_fourcc('D', 'I', 'V', 'X')
-    # fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
     fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-    # fourcc = cv2.VideoWriter_fourcc(*'MJPG')
-    # fourcc = cv2.VideoWriter_fourcc('H','2','6','4')
     video_out = cv2.VideoWriter(dest, fourcc, 20.0, (int(vid.get(cv2.CAP_PROP_FRAME_WIDTH)), int(vid.get(cv2.CAP_PROP_FRAME_HEIGHT))))
 
     if not vid.isOpened():
@@ -115,13 +131,6 @@ def detectLpVideo():
             # Tính thời gian cần chờ giữa các frame
             wait_time = int(1000 / frame_rate)
 
-            # new_frame_time = time.time()
-            # fps = 1/(new_frame_time-prev_frame_time)
-            # prev_frame_time = new_frame_time
-            # fps = int(fps)
-            # cv2.putText(frame, str(fps), (7, 70), cv2.FONT_HERSHEY_SIMPLEX, 3, (100, 255, 0), 3, cv2.LINE_AA)
-            # cv2.imshow('frame', frame)
-            # video_out.write(frame)
             if cv2.waitKey(wait_time) & 0xFF == ord('q'):
                 break
 
@@ -136,99 +145,6 @@ def detectLpVideo():
         cv2.destroyAllWindows()
 
         print('running 3 ...')
-
-        # Load the MP4 video
-        # video = VideoFileClip(dest)
-
-        # # Set the output GIF file name and duration (in seconds)
-        output_file = os.path.join(
-        "temp",
-        str(int(time.time() * 1_000_000)) + "-out." + "gif",
-        )
-        # duration = video.duration
-
-        # Set the start and end times for the GIF (in seconds)
-        # start_time = 0
-        # end_time = video.duration  # Set this to the desired duration
-
-        # Convert a subclip of the video to GIF
-        # video.subclip(start_time, end_time).to_gif(output_file)
-
-        # # Convert the video to GIF
-        # video.to_gif(output_file, duration=duration)
-        # print(f"Video converted to {output_file}")
-
-
-        # frames = imageio.imread(dest) 
-        # gif_image = pygifsicle.optimize(frames)
-        # with open('temp/out.gif', 'wb') as f: 
-        #     f.write(gif_image) 
-
-        # cap = cv2.VideoCapture(dest)
-        # fps = cap.get(cv2.CAP_PROP_FPS)
-        # start_time = 20*fps
-        # end_time = 25*fps
-        # image_lst = []
-        # i = 0
-        
-        # while True:
-        #     ret, frame = cap.read()
-        #     if ret == False:
-        #         break
-        #     if (i>=start_time and i<=end_time):
-        #         frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        #         image_lst.append(frame_rgb)
-        
-        #         cv2.imshow('a', frame)
-        #         key = cv2.waitKey(1)
-        #         if key == ord('q'):
-        #             break
-        #     i +=1
-        
-        # cap.release()
-        # cv2.destroyAllWindows()
-        
-        # dest_out = 'temp/output.gif'
-        # imageio.mimsave(dest_out, image_lst, fps=60)
-
-        # time.sleep(5)
-
-        # Đọc nội dung video từ tệp (ví dụ: video.mp4)
-        # with open(dest, 'rb') as video_file:
-        #     video_data = video_file.read()
-        
-        # # Tạo một đối tượng Response với MIME type là video/mp4
-        # response = Response(video_data, content_type='video/mp4')
-
-        # Open the video file
-        # cap = cv2.VideoCapture(dest)
-        
-        # # Check if the video file opened successfully
-        # if not cap.isOpened():
-        #     return "Error: Unable to open video file."
-
-        # def generate_frames():
-        #     while True:
-        #         success, frame = cap.read()
-        #         if not success:
-        #             break
-        #         ret, buffer = cv2.imencode('.jpg', frame)
-        #         if not ret:
-        #             break
-        #         frame = buffer.tobytes()
-        #         yield (b'--frame\r\n'
-        #             b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-
-        # return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
-
-        # time.sleep(5)
-
-        # Read the GIF file as binary data
-        # with open(output_file, "rb") as gif_file:
-        #     gif_binary_data = gif_file.read()
-
-        # Encode the binary data as base64
-        # base64_string = base64.b64encode(gif_binary_data).decode()
         
         # return send_file(output_file, mimetype='video/mp4')
         # return jsonify({
@@ -255,9 +171,31 @@ def detectLpVideo():
             print(f"Lỗi khi chuyển đổi: {e}")
 
         if os.path.exists(dest_out):
-            return send_file(dest_out, mimetype='video/mp4')
+            # data = { 'lps':  list(list_read_plates)}
+            new_lps = ', '.join(str(item) for item in list_read_plates)
+            # new_license_plate = LicensePlate(
+            #     lps=new_lps,
+            #     title='video',
+            #     queue_id=queue_id
+            # )
+            print('lps', new_lps)
+
+            try:
+                # db.session.add(new_license_plate)
+                cur.execute("INSERT INTO license_plates (title, lps, queue_id) VALUES (%s,%s,%s)",('video',new_lps,queue_id))    
+
+                db.commit()
+                return send_file(dest_out, mimetype='video/mp4')
+            except Exception as e:
+                print(f'Lỗi khi thêm bản ghi mới: {str(e)}')
+                db.rollback()
+                return jsonify({'message': 'Lỗi khi thêm bản ghi mới'}), 500
+            finally:
+                db.close()
         else:
             return jsonify({"error": "Video file not found"}), 404
+        
+        
     except Exception as e:
         print("Occur a Error" + str(e))
 
